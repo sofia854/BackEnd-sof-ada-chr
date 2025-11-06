e237da39235c8c5c9689bc6b0089b324d179847a
sofia854 <alvaradosofia915@gmail.com>
2025-11-06 17:36:27 +0000

    Endpoints de ejemplo

---
+++ api/src/main/java/mx/tecnm/backend/api/controller/Test.java
@@ -0,0 +1,49 @@
+package mx.tecnm.backend.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import mx.tecnm.backend.api.models.Producto;
+
+@RequestMapping("/test")
+@RestController
+public class Test {
+
+ @GetMapping("/hello")
+    public String helloworld(){
+        return "Hola api rest";
+    }
+
+    @GetMapping("/producto")
+public Producto getProducto(){
+Producto p = new Producto();
+p.nombre = "Coca Cola";
+p.precio=18.5;
+p.codigoBarras="12345678";
+return p;
+
+}
+
+
+@GetMapping("/productos")
+public Producto[] getProductos() {
+    Producto p1 = new Producto();
+    p1.nombre = "Coca Cola";
+    p1.precio = 18.5;
+    p1.codigoBarras = "7501055312306";
+
+Producto p2 = new Producto();
+p2. nombre = "Pepsi";
+p2.precio = 17.5;
+p2.codigoBarras = "7501055312307";
+
+Producto p3 = new Producto();
+p3. nombre = "Fanta";
+p3.precio = 16.5;
+p3.codigoBarras = "7501055312308";
+
+return new Producto[]{p1, p2, p3};
+}
+
+}
---
+++ api/src/main/java/mx/tecnm/backend/api/models/Producto.java
@@ -0,0 +1,8 @@
+package mx.tecnm.backend.api.models;
+
+public class Producto {
+  public  String nombre;
+    public double precio;
+    public String codigoBarras;
+
+}
