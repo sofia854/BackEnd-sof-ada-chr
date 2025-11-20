0fba0ea740ee7c03d22f920449af19f1b1ad8d9e
sofia854 <alvaradosofia915@gmail.com>
2025-11-20 17:58:25 +0000

    Coneccion de BD

--- api/src/main/java/mx/tecnm/backend/api/controller/Test.java
+++ api/src/main/java/mx/tecnm/backend/api/controller/Test.java
@@ -1,6 +1,10 @@
 package mx.tecnm.backend.api.controller;
 
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +29,6 @@ return p;
 
 }
 
-
 @GetMapping("/productos")
 public Producto[] getProductos() {
     Producto p1 = new Producto();
@@ -44,6 +47,39 @@ p3.precio = 16.5;
 p3.codigoBarras = "7501055312308";
 
 return new Producto[]{p1, p2, p3};
+}
+
+
+@GetMapping("/productos/{id}")
+public ResponseEntity<Producto> buscarPorid(@PathVariable int id){
+
+    if (id < 0 || id > 2) 
+    {
+        return ResponseEntity.notFound().build();    
+    }
+
+    Producto[] productos = new Producto[3];
+    Producto p1 = new Producto();
+    p1.nombre = "Coca Cola";
+    p1.precio = 18.5;
+    p1.codigoBarras = "7501055312306";
+    productos[0] = p1;
+
+    Producto p2 = new Producto();
+    p2. nombre = "Pepsi";
+    p2.precio = 17.5;
+    p2.codigoBarras = "7501055312307";
+    productos[1] = p2;
+
+   Producto p3 = new Producto();
+   p3. nombre = "Fanta";
+   p3.precio = 16.5;
+   p3.codigoBarras = "7501055312308";
+   productos[2] = p3;  
+
+   Producto resultado = productos[id];
+return  ResponseEntity.ok(resultado);
+
 }
 
 }
