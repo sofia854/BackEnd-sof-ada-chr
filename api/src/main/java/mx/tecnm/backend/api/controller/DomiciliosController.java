f67e6f9eeb1537ce77969cc5fd7049358a8cd36a
sofia854 <alvaradosofia915@gmail.com>
2025-12-03 00:00:34 +0000

    CRUD de Domicilios

--- api/src/main/java/mx/tecnm/backend/api/controller/DomiciliosController.java
+++ api/src/main/java/mx/tecnm/backend/api/controller/DomiciliosController.java
@@ -3,8 +3,14 @@ package mx.tecnm.backend.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.Delimiter;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +23,61 @@ public class DomiciliosController {
     
 @Autowired
 DomicilioDAO repo;
+
+//Obtener TODOS los domicilios
 @GetMapping()
 public ResponseEntity<List<Domicilio>> obtenerProductos() {
     // Lógica para obtener la lista de domicilios
  List<Domicilio> domicilios = repo.consultarDomicilios();
  return ResponseEntity.ok(domicilios);
 } 
+//Obtener domicilio por ID
+@GetMapping("/{id}")
+public ResponseEntity<Domicilio> obtenerDomicilioPorId(@PathVariable int id) {
+Domicilio domicilio = repo.consultarDomicilioPorId(id);
+if (domicilio == null) {
+    return ResponseEntity.notFound().build();
+
+}
+return ResponseEntity.ok(domicilio);
     }
+
+    //Crear Domicilio
+    @PostMapping
+    public ResponseEntity<String> agregarDomicilio(@RequestBody Domicilio domicilio) {
+
+        int filas = repo.insertarDomicilio(domicilio);
+
+        if (filas == 1) {
+            return ResponseEntity.ok("Domicilio agregado correctamente :)");
+            
+        } else {
+            return ResponseEntity.badRequest().body("No se pudo agregar el domicilio :(");
+        }
+    }
+
+//Modificar Domicilio
+@PutMapping("/{id}")
+public ResponseEntity<String> modificarDomicilio(@PathVariable int id, @RequestBody Domicilio domicilio) {
+    int filas = repo.modificarDomicilio(domicilio, id);
+    if (filas == 1) {
+        return ResponseEntity.ok("Domicilio modificado correctamente :)");
+    } else {
+        return ResponseEntity.badRequest().body("No se pudo modificar el domicilio :(");
+    }
+}
+
+
+//Eliminar Domicilio
+
+@DeleteMapping("/{id}")
+public ResponseEntity<String> eliminarDomicilio(@PathVariable int id) {
+    int filas = repo.eliminarDomicilio(id);
+    if (filas == 1) {
+        return ResponseEntity.ok("Domicilio eliminado correctamente :)");
+    } else {
+        return ResponseEntity.badRequest().body("No se pudo eliminar el domicilio :(");
+    }
+
+}
+}
--- api/src/main/java/mx/tecnm/backend/api/repository/DomicilioDAO.java
+++ api/src/main/java/mx/tecnm/backend/api/repository/DomicilioDAO.java
@@ -30,4 +30,74 @@ public class DomicilioDAO {
         .list();
     }
     
+//Obtener por id 
+public Domicilio consultarDomicilioPorId(int id) {
+    String sql = "SELECT id, calle, numero, colonia, cp, estado, ciudad, usuarios_id FROM domicilios WHERE id = ?";
+
+    return conexion.sql(sql)
+            .params(id)
+            .query((rs, rowNum) -> new Domicilio(
+                rs.getInt("id"),
+                rs.getString("calle"),
+                rs.getInt("numero"),
+                rs.getString("colonia"),
+                rs.getString("cp"),
+                rs.getString("estado"),
+                rs.getString("ciudad"),
+                rs.getInt("usuarios_id")
+            ))
+            .optional()
+            .orElse(null);
+
+        }
+
+        //Insertar domicilio
+        public int insertarDomicilio(Domicilio domicilio) {
+            String sql = """
+                INSERT INTO domicilios(calle, numero, colonia, cp, estado, ciudad, usuarios_id)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                """;
+            return conexion.sql(sql)
+                .params(
+                    domicilio.calle(),
+                    domicilio.numero(),
+                    domicilio.colonia(),
+                    domicilio.cp(),
+                    domicilio.estado(),
+                    domicilio.ciudad(),
+                    domicilio.usuarios_id()
+                )
+                .update();  
+            }
+
+//Modificar domicilio
+public int modificarDomicilio( Domicilio domicilio, int id) {
+    String sql = """
+        UPDATE domicilios
+        SET calle = ?, numero = ?, colonia = ?, cp = ?, estado = ?, ciudad = ?, usuarios_id = ?
+        WHERE id = ?
+        """;
+
+    return conexion.sql(sql)
+        .params(
+            domicilio.calle(),
+            domicilio.numero(),
+            domicilio.colonia(),
+            domicilio.cp(),
+            domicilio.estado(),
+            domicilio.ciudad(),
+            domicilio.usuarios_id(),
+            id
+        )
+        .update();
+    }
+
+    //Eliminar domicilio
+    public int eliminarDomicilio(int id) {
+        String sql = "DELETE FROM domicilios WHERE id = ?";
+        return conexion.sql(sql)
+            .params(id)
+            .update();
+    }
+
 }
